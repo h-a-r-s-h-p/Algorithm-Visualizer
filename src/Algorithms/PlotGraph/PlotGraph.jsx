d1d94e71692fa8d460a469004c7f680bbfcb9af9
h-a-r-s-h-p <111801015@smail.iitpkd.ac.in>
2022-03-13 11:44:01 +0530

    graph plotted

--- src/Algorithms/PlotGraph/PlotGraph.jsx
+++ src/Algorithms/PlotGraph/PlotGraph.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import { DataSet, Network} from 'vis-network/standalone/esm/vis-network';
+import "./PlotGraph.css"
 
 export default function PlotGraph(props) {
+
+  const domNode = useRef(null);
+
+  const network = useRef(null);
+
   var vertexCount = props.graph.vertexCount;
   vertexCount = parseInt(vertexCount);
   console.log("type of vertexCount is : "+ typeof vertexCount)
@@ -20,9 +27,43 @@ export default function PlotGraph(props) {
       label: i.toString(),
     }
   }
-  var nodes = new vis.dat
+
+  var edges = new Array();
+  for(var i =0; i<edgeCount;i++){
+    edges[i]={
+      from: startingVertices[i],
+      to: endingVertices[i],
+      arrows: {
+        to: {
+          enabled: true,
+          type: "arrow",
+        },
+      },
+      label: weights[i],
+      font:{
+        align: "top"
+      }
+    };
+  }
+
+  var nodes = new DataSet(nodesArray);
+  edges = new DataSet(edges);
+
+  const data = {
+    nodes,
+    edges
+  };
+
+  const options ={}
+
+  useEffect(
+    () => {
+      network.current = new Network(domNode.current, data, options);
+    },
+    [domNode, network, data, options ]
+  );
 
   return (
-    <div id = "graph">Graph</div>
-  )
-}
+    <div className="thegraph" ref = {domNode} />
+  );
+};
